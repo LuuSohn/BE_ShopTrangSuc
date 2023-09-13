49713ced9d78e2cac976be9afd7ad060f3a574eb
LuuSohn <luuhoangson2002@gmail.com>
2023-09-13 11:25:57 +0700

    .

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,7 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/category')
-const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
 
-
-router.post('/', [verifyAccessToken, isAdmin], ctrls.createdCategory)
-router.get('/',  ctrls.getAllCategory)
-router.put('/:pcid', [verifyAccessToken, isAdmin], ctrls.updateCategory)
-router.delete('/:pcid', [verifyAccessToken, isAdmin], ctrls.deleteCategory)
 
 
 
